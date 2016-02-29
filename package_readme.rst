1850bec8ebc39942e339d2f1864dfb689ba89674
adriant <adriant@catalyst.net.nz>
2016-02-29 15:36:37 +1300

    Updating package details, version to 0.1.2
    * Changing development status to Production/Stable
    * Version set to 0.1.2
    * Adding package description
    * Adding conf dir to package
    
    Change-Id: Ia41714ac9bafa726af62a69a1565a97d62b4e48d

---
+++ package_readme.rst
@@ -0,0 +1,19 @@
+StackTask is a service that sits along Keystone and allows the automation and approval of tasks normally requiring a user with an admin role. StackTask allows defining of such tasks as part of a workflow which can either be entirely automatic, or require admin approval. The goal is to automate business logic, and augment the functionality of Keystone and other OpenStack services without getting in the way of future OpenStack features or duplicating development effort.
+
+Quick Dev Deployment
+====================
+
+To quickly deploy the service for testing you can install via pip, setup a default config file, and then run the test Django server.
+
+::
+
+    pip install stacktask
+
+Then running the service will look for a config in either **/etc/stacktask/conf.yaml** or it will default to **conf/conf.yaml** from the directory you run the command in.
+
+::
+
+    stacktask migrate
+    stacktask runserver <port>
+
+For now you will have to source the default conf from the github repo or the library install location itself, but we hope to add an additional commandline function which will copy and setup a basic default config in **/etc/stacktask/conf.yaml**.
--- setup.py
+++ setup.py
@@ -3,27 +3,30 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.readlines()
 
+with open('package_readme.rst') as file:
+    long_description = file.read()
+
 setup(
     name='stacktask',
 
-    version='0.1.1a4',
-    description='A user task service for openstack.',
-    long_description=(
-        'A task service to sit alongside keystone and ' +
-        'add some missing functionality.'),
+    version='0.1.2',
+    description='An admin task workflow service for openstack.',
+    long_description=long_description,
     url='https://github.com/catalyst/stacktask',
     author='Adrian Turjak',
     author_email='adriant@catalyst.net.nz',
     license='Apache 2.0',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: Apache Software License',
+        'Framework :: Django :: 1.8',
         'Programming Language :: Python :: 2.7',
+        'Environment :: OpenStack',
     ],
 
     keywords='openstack keystone users tasks registration workflow',
-
     packages=find_packages(),
     package_data={
         'stacktask': [
